a6a164d6e567d54bad28f70dfc137dc2aa65a4a6
Hasmica <145840800+Hasmica@users.noreply.github.com>
2024-07-21 12:45:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import openai
+import time
+
+def main():
+    client = openai
+    assistant_id = "asst_TufcOXOqdLpyIYQIPNkW4E1Y"
+
+    openai.api_key="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"
+
+    if "start_chat" not in st.session_state:
+        st.session_state.start_chat = False
+    if "thread_id" not in st.session_state:
+        st.session_state.thread_id = None
+    if st.sidebar.button("Start chat"):
+        st.session_state.start_chat = True
+        thread = client.beta.threads.create()
+        st.session_state.thread_id = thread.id
+    
+    st.title("Banking Chatbot")
+    if st.sidebar.button("Clear chat"):
+        st.session_state.messages = []
+        st.session_state.start_chat = False
+        st.session_state.thread_id = None
+    
+    if st.session_state.start_chat:
+        if "openai_model" not in st.session_state:
+            st.session_state.openai_model = "gpt-4-1106-preview"
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+        
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+        
+        if prompt := st.chat_input("Ask your question here"):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            
+            client.beta.threads.messages.create(
+                thread_id=st.session_state.thread_id,
+                role="user",
+                content=prompt
+            )
+            
+            run = client.beta.threads.runs.create(
+                thread_id=st.session_state.thread_id,
+                assistant_id=assistant_id,
+                instructions= "You are a helpful assistant who can answer any question related to banking policies based on the given documents. If you are not able to find the answer, please respond as answer not found"
+            )
+
+            while run.status != "completed":
+                time.sleep(1)
+                run = client.beta.threads.runs.retrieve(
+                    thread_id=st.session_state.thread_id,
+                    run_id=run.id
+                )
+            messages = client.beta.threads.messages.list(
+                thread_id=st.session_state.thread_id
+            )
+
+            assistant_messages = [
+                message for message in messages
+                if message.run_id == run.id and message.role == "assistant"
+            ]
+            for message in assistant_messages:
+                st.session_state.messages.append({"role": "assistant", "content": message.content[0].text.value})
+                with st.chat_message("assistant"):
+                    st.markdown(message.content[0].text.value)
+    else:
+        st.write("Click start chat to begin")
+
+main()
